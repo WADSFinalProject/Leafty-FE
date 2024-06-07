e073830815907269add091ac30de0d8e4ea1c27f
Joseph Ruys <93302274+Tropicaljittai@users.noreply.github.com>
2024-06-07 15:11:05 +0700

    added login and register complete function (needs 2nd auth and encryption)

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -3,6 +3,8 @@ import illustration from "@assets/404.svg";
 import {db, auth} from '../firebase';
 import { signOut, onAuthStateChanged } from "firebase/auth";
 import { useLocation, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { API_URL } from '../App';
 
 function PageNotFound() {
     const navigate = useNavigate();
@@ -16,9 +18,29 @@ function PageNotFound() {
             alert(err.message);
           });
       };
+
+      async function handleWhoAmI() {
+        try {
+            const response = await axios.get(API_URL + "/whoami")
+            console.log("who am i (UUID) : ", response.data.user_id)
+            if (response) {
+                return response.data.user_id
+            }
+            return false
+        } catch (error) {
+            console.error("Error while checking session:", error);
+            console.error(err.response.data);    // ***
+            console.error(err.response.status);  // ***
+            console.error(err.response.headers);
+            return false
+
+        }
+    }
+
     return (
         <div className='flex flex-col justify-center items-center min-h-screen'>
             <button onClick={handleSignOut}>sign out button for now</button>
+            <button onClick={handleWhoAmI}>whoami</button>
             <img src={illustration} className='w-[500px] h-[500px]' alt="Page Not Found Illustration"/>
             <span className='font-bold text-3xl mt-4'>Oops! Page Not Found</span>
             <span className='w-1/3 text-center mt-2'>We've searched far and wide and couldn't seem to find what you were looking for</span>
