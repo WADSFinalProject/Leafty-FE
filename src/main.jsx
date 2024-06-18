fda6fcda99328e73de0d06130b45f0134313df3a
Davin neilson <davin.neilson@binus.ac.id>
2024-06-18 21:10:36 +0700

    ezzz cuy

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx'
 import './style/index.css'
 import './style/font.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import axios from 'axios'
+
+axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
